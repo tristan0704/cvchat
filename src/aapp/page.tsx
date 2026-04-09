d70a5fba76b5606715de0e806bd49566e1d80f55
Tristan Trunez <tristan.trunez@outlook.de>
2026-04-09 18:43:03 +0200

    delete old code

--- src/aapp/page.tsx
+++
@@ -1,7 +0,0 @@
-﻿// DATEIUEBERSICHT: Startseite, die initial auf den passenden Bereich weiterleitet.
-import { redirect } from "next/navigation"
-
-export default function RootPage() {
-  redirect("/home")
-}
-
